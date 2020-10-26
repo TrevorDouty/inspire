dfdc4fffa3810c29f24c1813570537a385f41b3d
TrevorDouty <tdouty@nnu.edu>
2020-10-25 23:44:44 -0600

    changed styling

--- app/models/Todo.js
+++ app/models/Todo.js
@@ -9,8 +9,8 @@ export default class Todo {
 
   get TodoTemplate() {
     return /*html*/`
-${this.Color} <button class = "text-success close" onclick="app.todoController.toggleTodoStatus('${this.id}')"><span>&#10003;</span></button>
-<button class="text-danger close float-right" onclick="app.todoController.removeTodo('${this.id}')"> <span>&times;</span> </button>
+<section class="d-flex">${this.Color} <button class = "text-success close" onclick="app.todoController.toggleTodoStatus('${this.id}')"><span><b>&#10003;</b></span></button>
+<button class="text-danger close float-right" onclick="app.todoController.removeTodo('${this.id}')"> <span>&times;</span> </button></section>
 `
   }
 
@@ -18,9 +18,9 @@ ${this.Color} <button class = "text-success close" onclick="app.todoController.t
   get Color() {
 
     if (this.completed) {
-      return `<p class='text-green'>${this.description}</p>`
+      return `<h4 class='text-green'>${this.description}</h4>`
     } else {
-      return `<p class='text-red'>${this.description}</p>`
+      return `<h4 class='text-red'>${this.description}</h4>`
     }
 
   }
